4775d50f6b4c4fef2cd190a687f64d1b39871a20
zjoney <zdjoey@126.com>
2023-02-01 14:51:22 +0800

    feat: 🎸 repro

---
+++ .umirc.ts
@@ -0,0 +1,66 @@
+import { readdirSync } from 'fs';
+import { defineConfig } from 'dumi';
+import { join } from 'path';
+
+const headPkgList = [];
+const pkgList = readdirSync(join(__dirname, 'packages')).filter(
+  (pkg) => pkg.charAt(0) !== '.' && !headPkgList.includes(pkg),
+);
+const tailPkgList = pkgList
+  .map((path) => [join('packages', path, 'src')])
+  .reduce((acc, val) => acc.concat(val), []);
+
+const alias = pkgList.reduce((pre, pkg) => {
+  pre[`@ant-design/pro-${pkg}`] = join(__dirname, 'packages', pkg, 'src');
+  return {
+    ...pre,
+  };
+}, {});
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+export default defineConfig({
+  title: 'EW-Component',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  resolve: {
+    includes: [...tailPkgList, 'docs'],
+  },
+  locales: [
+    ['zh-CN', '中文'],
+    ['en-US', 'English'],
+  ],
+  navs: {
+    'en-US': [
+      null,
+      {
+        title: 'GitHub',
+        path: 'https://github.com/ant-design/pro-components',
+      },
+    ],
+    'zh-CN': [
+      null,
+      {
+        title: 'GitHub',
+        path: 'https://github.com/ant-design/pro-components',
+      },
+    ],
+  },
+  analytics: isProduction
+    ? {
+        ga: 'UA-173569162-1',
+      }
+    : false,
+  hash: true,
+  targets: {
+    chrome: 80,
+    firefox: false,
+    safari: false,
+    edge: false,
+    ios: false,
+  },
+  // more config: https://d.umijs.org/config
+});
---
+++ packages/button/src/button.md
@@ -0,0 +1,32 @@
+---
+title: Button 按钮
+nav:
+  title: 组件
+  path: /components
+group:
+  path: /
+---
+
+# Button 按钮
+
+## 代码演示
+
+### 按钮类型
+
+<code src="./demos/type.tsx"  title="按钮类型"  />
+
+### 按钮大小
+
+<code src="./demos/size.tsx"  title="按钮大小"   />
+
+### 按钮形状
+
+<code src="./demos/shape.tsx"  title="按钮形状"   />
+
+## API
+
+| 参数 | 说明 | 类型 | 默认值 |
+| --- | --- | --- | --- |
+| shape | 按钮形状 | `circle` \| `round` | `round` |
+| type | 按钮类型 | `primary` \| `ghost` \| `dashed` \| `link` \| `text` \| `default` | `default` |
+| children | 插槽 | `React.ReactNode` | - |
